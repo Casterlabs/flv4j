6cc7de5f5613efdbd4e5f90ce174bb372b3d1833
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-06-30 11:37:38 -0500

    Clean up the RTMP Reader class.
    - nested classes
    - chunkStream LUT instead of HashMap.

--- src/main/java/co/casterlabs/flv4j/rtmp/RTMPReader.java
+++ src/main/java/co/casterlabs/flv4j/rtmp/RTMPReader.java
@@ -1,11 +1,12 @@
 package co.casterlabs.flv4j.rtmp;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+
+import org.jetbrains.annotations.Nullable;
 
 import co.casterlabs.flv4j.actionscript.io.ASReader;
 import co.casterlabs.flv4j.rtmp.chunks.RTMPChunk;
+import co.casterlabs.flv4j.rtmp.chunks.RTMPMessage;
 import co.casterlabs.flv4j.rtmp.chunks.RTMPMessageAbort;
 import co.casterlabs.flv4j.rtmp.chunks.RTMPMessageChunkSize;
 import co.casterlabs.flv4j.rtmp.chunks.RTMPMessageWindowAcknowledgementSize;
@@ -19,7 +20,7 @@ import lombok.Setter;
 @RequiredArgsConstructor
 public class RTMPReader {
     private final ASReader reader;
-    private final Map<Integer, ChunkStream> chunkStreams = new HashMap<>();
+    private ChunkStream[] chunkStreams = {}; // NB: [0] and [1] are illegal values (they indicate extended id format)
 
     int previousTimestamp;
     private int chunkSize = 128;
@@ -75,10 +76,14 @@ public class RTMPReader {
             this.incrementRead(2);
         }
 
-        ChunkStream cs = this.chunkStreams.get(csId);
+        if (csId >= this.chunkStreams.length) {
+            ChunkStream[] newArr = new ChunkStream[csId + 1];
+            System.arraycopy(this.chunkStreams, 0, newArr, 0, this.chunkStreams.length);
+            this.chunkStreams = newArr;
+        }
+        ChunkStream cs = this.chunkStreams[csId];
         if (cs == null) {
-            cs = new ChunkStream(this, this.reader);
-            this.chunkStreams.put(csId, cs);
+            cs = this.chunkStreams[csId] = new ChunkStream();
         }
 
         RTMPChunk<?> chunk = cs.read(format, csId, this.chunkSize);
@@ -89,9 +94,9 @@ public class RTMPReader {
         this.previousTimestamp = chunk.timestamp();
 
         if (chunk.message() instanceof RTMPMessageAbort abort) {
-            ChunkStream stream = this.chunkStreams.get((int) abort.streamId());
+            ChunkStream stream = this.chunkStreams[(int) abort.streamId()];
             if (stream != null) {
-                stream.abort();
+                stream.inProgress = null;
             }
             return null;
         } else if (chunk.message() instanceof RTMPMessageChunkSize chunkMessage) {
@@ -101,6 +106,146 @@ public class RTMPReader {
         }
 
         return chunk;
+    }
+
+    class ChunkStream {
+        private int previousMessageLength;
+        private int previousMessageTypeId;
+        private long previousMessageStreamId;
+
+        private ChunkInProgress inProgress;
+
+        @Nullable
+        RTMPChunk<?> read(int format, int csId, int chunkSize) throws IOException {
+            long timestamp;
+            int messageLength;
+            int messageTypeId;
+            long messageStreamId;
+            switch (format) {
+                case 0: {
+                    // https://rtmp.veriskope.com/pdf/rtmp_specification_1.0.pdf#page=14
+                    timestamp = reader.u24();
+                    messageLength = reader.u24();
+                    messageTypeId = reader.u8();
+                    messageStreamId = reader.u32le();
+
+                    incrementRead(11);
+
+                    if (timestamp == 0xFFFFFF) {
+                        timestamp = reader.u32();
+                        incrementRead(4);
+                    }
+                    break;
+                }
+
+                case 1: {
+                    // https://rtmp.veriskope.com/pdf/rtmp_specification_1.0.pdf#page=14
+                    // (reuse messageStreamId)
+                    messageStreamId = this.previousMessageStreamId;
+
+                    long timestampDelta = reader.u24();
+                    messageLength = reader.u24();
+                    messageTypeId = reader.u8();
+
+                    incrementRead(7);
+
+                    if (timestampDelta == 0xFFFFFF) {
+                        timestampDelta = reader.u32();
+                        incrementRead(4);
+                    }
+
+                    timestamp = previousTimestamp + timestampDelta;
+                    break;
+                }
+
+                case 2: {
+                    // https://rtmp.veriskope.com/pdf/rtmp_specification_1.0.pdf#page=15
+                    // (reuse everything except timestamp)
+                    messageLength = this.previousMessageLength;
+                    messageTypeId = this.previousMessageTypeId;
+                    messageStreamId = this.previousMessageStreamId;
+
+                    long timestampDelta = reader.u24();
+
+                    incrementRead(3);
+
+                    if (timestampDelta == 0xFFFFFF) {
+                        timestampDelta = reader.u32();
+                        incrementRead(4);
+                    }
+
+                    timestamp = previousTimestamp + timestampDelta;
+                    break;
+                }
+
+                case 3:
+                    // https://rtmp.veriskope.com/pdf/rtmp_specification_1.0.pdf#page=15
+                    // (reuse all previous values)
+                    timestamp = previousTimestamp;
+                    messageLength = this.previousMessageLength;
+                    messageTypeId = this.previousMessageTypeId;
+                    messageStreamId = this.previousMessageStreamId;
+                    break;
+
+                default: // Silence the compiler.
+                    throw new IllegalStateException();
+            }
+
+            int timestamp31 = (int) (timestamp & 0x7FFFFFFFL);
+
+            previousTimestamp = timestamp31;
+            this.previousMessageLength = messageLength;
+            this.previousMessageTypeId = messageTypeId;
+            this.previousMessageStreamId = messageStreamId;
+
+            // 2 is the abort message, we need to parse that FULLY even if the stream is in
+            // the middle of a chunk.
+
+            RTMPMessage message;
+            if (messageLength > chunkSize && messageTypeId != 2) {
+                if (this.inProgress == null) {
+                    this.inProgress = new ChunkInProgress(messageLength);
+                }
+
+                int maxToRead = Math.min(chunkSize, this.inProgress.remaining());
+                incrementRead(maxToRead);
+                if (this.inProgress.append(reader.bytes(maxToRead))) {
+                    return null;
+                }
+
+                message = RTMPMessage.parse(messageTypeId, this.inProgress.buffer.length, new ASReader(this.inProgress.buffer));
+                this.inProgress = null;
+            } else {
+                message = RTMPMessage.parse(messageTypeId, messageLength, reader);
+            }
+
+            return new RTMPChunk<>(timestamp31, csId, messageTypeId, messageStreamId, message);
+        }
+
+    }
+
+    private static class ChunkInProgress {
+        final byte[] buffer;
+        private int writeOffset = 0;
+
+        ChunkInProgress(int length) {
+            this.buffer = new byte[length];
+        }
+
+        int remaining() {
+            return this.buffer.length - this.writeOffset;
+        }
+
+        /**
+         * @return true if more data is needed.
+         */
+        boolean append(byte[] bytes) {
+            System.arraycopy(bytes, 0, this.buffer, this.writeOffset, bytes.length);
+            this.writeOffset += bytes.length;
+
+            return this.writeOffset < this.buffer.length;
+        }
+
     }
 
 }
