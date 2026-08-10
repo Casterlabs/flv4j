c97cf126d27732a751b8018dffb5c2a9638bb816
e3ndr <33337309+e3ndr@users.noreply.github.com>
2026-08-10 08:46:59 -0500

    Make video still extend RawSerializable.

--- src/main/java/co/casterlabs/flv4j/flv/tags/video/ex/FLVExVideoCodecData.java
+++ src/main/java/co/casterlabs/flv4j/flv/tags/video/ex/FLVExVideoCodecData.java
@@ -1,7 +1,8 @@
 package co.casterlabs.flv4j.flv.tags.video.ex;
 
+import co.casterlabs.flv4j.FLVBVRawSerializable;
 import co.casterlabs.flv4j.codecs.VideoCodecData;
 
-public interface FLVExVideoCodecData extends VideoCodecData {
+public interface FLVExVideoCodecData extends VideoCodecData, FLVBVRawSerializable {
 
 }
